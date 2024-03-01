c1172d73ff6c7a7600e5434cdd9725677d9decf3
jcsales <juancarlos.sales@vedruna.es>
2024-03-01 15:45:52 +0100

    Cuarto commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("hola mundo");
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+            System.out.println();
         }
 
     }
